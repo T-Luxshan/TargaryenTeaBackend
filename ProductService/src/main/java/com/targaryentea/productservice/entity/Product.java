66b4cbf6ae3a1f3c765bbfe19b51ea707de171c9
T-Luxshan <luckybraveboys@gmail.com>
2024-11-16 21:37:36 +0530

    Product entity created

--- ProductService/src/main/java/com/targaryentea/productservice/entity/Product.java
+++ ProductService/src/main/java/com/targaryentea/productservice/entity/Product.java
@@ -1,4 +1,26 @@
 package com.targaryentea.productservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+    private String image_url;
+
 }
