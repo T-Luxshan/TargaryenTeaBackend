9726fb2f2b744581ca32cb097b06080d0b48efae
T-Luxshan <luckybraveboys@gmail.com>
2024-11-23 00:08:41 +0530

    getAllProducts, deleteProduct, getProductById added

--- ProductService/src/main/java/com/targaryentea/productservice/controller/ProductController.java
+++ ProductService/src/main/java/com/targaryentea/productservice/controller/ProductController.java
@@ -6,10 +6,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/product")
@@ -22,5 +21,19 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<String> createProduct(@Valid @RequestBody ProductRequest productRequest){
         return ResponseEntity.ok(productService.createProduct(productRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductRequest>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+        return ResponseEntity.ok(productService.deleteProduct(id));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ProductRequest> getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
     }
 }
--- ProductService/src/main/java/com/targaryentea/productservice/service/ProductService.java
+++ ProductService/src/main/java/com/targaryentea/productservice/service/ProductService.java
@@ -7,6 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,9 +32,47 @@ public class ProductService {
                     .build();
             productRepository.save(product);
             return "Product Created";
+    }
 
+    public List<ProductRequest> getAllProducts() {
+        List<Product> products = productRepository.findAll();
 
+        ArrayList<ProductRequest> productRequests = new ArrayList<>();
 
+        ProductRequest productRequest;
+        for (Product product: products) {
+            productRequest = ProductRequest.builder()
+                    .id(product.getId())
+                    .name(product.getName())
+                    .description(product.getDescription())
+                    .price(product.getPrice())
+                    .stock(product.getStock())
+                    .image_url(product.getImage_url())
+                    .build();
+            productRequests.add(productRequest);
+        }
+        return productRequests;
+    }
+
+    public String deleteProduct(Long id) {
+        productRepository.deleteById(id);
+        return "Product deleted";
+    }
+
+    public ProductRequest getProductById(Long id) {
+        Optional<Product> productTemp = productRepository.findById(id);
+        if(productTemp.isPresent()){
+            Product product = productTemp.get();
+            return ProductRequest.builder()
+                    .id(product.getId())
+                    .name(product.getName())
+                    .description(product.getDescription())
+                    .price(product.getPrice())
+                    .stock(product.getStock())
+                    .image_url(product.getImage_url())
+                    .build();
+        }
+        return new ProductRequest();
 
     }
 }
