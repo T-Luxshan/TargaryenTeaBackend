ddfa09e658e074a17868f9bce8a56b9027ab7120
Suhira30 <suhirabalarajan@gmail.com>
2024-11-22 18:56:46 +0530

    setup

---
+++ src/main/java/com/example/targaryenteabackend/TargaryenTeaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.targaryenteabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargaryenTeaBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TargaryenTeaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/targaryenteabackend/TargaryenTeaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.targaryenteabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargaryenTeaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
