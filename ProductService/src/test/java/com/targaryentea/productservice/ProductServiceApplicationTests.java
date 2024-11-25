7a50c8bf66f374600710b2524af551d00e3c682d
T-Luxshan <luckybraveboys@gmail.com>
2024-11-25 17:09:56 +0530

    test dependency added

--- ProductService/src/test/java/com/targaryentea/productservice/ProductServiceApplicationTests.java
+++ ProductService/src/test/java/com/targaryentea/productservice/ProductServiceApplicationTests.java
@@ -2,9 +2,16 @@ package com.targaryentea.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+//@Testcontainers
 class ProductServiceApplicationTests {
+
+//    @Container
+//    static MySQLContainer mySQLContainer = new MySQLContainer();
 
     @Test
     void contextLoads() {
