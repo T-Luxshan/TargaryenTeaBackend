8baa996c05c52e62ddd55681ecd5a8c188c33aaa
T-Luxshan <luckybraveboys@gmail.com>
2024-11-28 00:05:53 +0530

    order entity created

---
+++ OrderService/src/main/java/com/targrayentea/orderservice/entity/Order.java
@@ -0,0 +1,22 @@
+package com.targrayentea.orderservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+}
