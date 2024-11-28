79829921e9031706a0d7d4ec8c613b6052708ebe
T-Luxshan <luckybraveboys@gmail.com>
2024-11-28 13:41:16 +0530

    GetAll order

--- OrderService/src/main/java/com/targrayentea/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/targrayentea/orderservice/controller/OrderController.java
@@ -2,10 +2,13 @@ package com.targrayentea.orderservice.controller;
 
 import com.targrayentea.orderservice.dto.OrderDTO;
 import com.targrayentea.orderservice.dto.OrderLineItemsDto;
+import com.targrayentea.orderservice.dto.OrderResponse;
 import com.targrayentea.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -15,12 +18,12 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<String> placeOrder(@RequestBody OrderDTO orderDTO){
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderDTO orderDTO){
         return ResponseEntity.ok(orderService.placeOrder(orderDTO));
     }
 
     @GetMapping
-    public  ResponseEntity<OrderDTO> getAllOrder(){
+    public  ResponseEntity<List<OrderDTO>> getAllOrder(){
         return ResponseEntity.ok(orderService.getAllOrder());
     }
 }
