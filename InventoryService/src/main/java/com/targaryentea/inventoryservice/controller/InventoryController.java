bd20f8b317147b1a80da62a0f65c7de9d47a6a84
T-Luxshan <luckybraveboys@gmail.com>
2024-11-29 00:09:50 +0530

    IsInStock in inventory

--- InventoryService/src/main/java/com/targaryentea/inventoryservice/controller/InventoryController.java
+++ InventoryService/src/main/java/com/targaryentea/inventoryservice/controller/InventoryController.java
@@ -1,6 +1,10 @@
 package com.targaryentea.inventoryservice.controller;
 
+import com.targaryentea.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
+
+    final private InventoryService inventoryService;
+    @GetMapping("{sku-code}")
+    public ResponseEntity<Boolean> isInStock(@PathVariable("sku-code") String skuCode){
+        return ResponseEntity.ok(inventoryService.isInStock(skuCode));
+    }
 }
--- InventoryService/src/main/java/com/targaryentea/inventoryservice/service/InventoryService.java
+++ InventoryService/src/main/java/com/targaryentea/inventoryservice/service/InventoryService.java
@@ -1,4 +1,24 @@
 package com.targaryentea.inventoryservice.service;
 
+import com.targaryentea.inventoryservice.entity.Inventory;
+import com.targaryentea.inventoryservice.repository.InventoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
 public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Boolean isInStock(String skuCode) {
+        Optional<Inventory> inventory =  inventoryRepository.findBySkuCode(skuCode);
+        return inventory.isPresent();
+    }
 }
