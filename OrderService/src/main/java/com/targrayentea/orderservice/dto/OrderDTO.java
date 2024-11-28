b830756edb5ab208fbd3023b856b756342d5566e
T-Luxshan <luckybraveboys@gmail.com>
2024-11-28 13:39:51 +0530

    OrderDTO updated

--- OrderService/src/main/java/com/targrayentea/orderservice/dto/OrderDTO.java
+++ OrderService/src/main/java/com/targrayentea/orderservice/dto/OrderDTO.java
@@ -1,17 +1,20 @@
 package com.targrayentea.orderservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrderDTO {
-
+    private Long id;
+    private String userId;
+    private String orderNumber;
+    private LocalDateTime orderDate;
     @NotBlank(message = "Product name cannot be empty")
     private List<OrderLineItemsDto> orderLineItemsDtoList;
 }
