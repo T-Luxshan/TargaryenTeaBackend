0ca399ec7502f64075526eac664a04839d71b5f7
T-Luxshan <luckybraveboys@gmail.com>
2024-11-30 15:39:40 +0530

    RequiredArgsConstructor

--- InventoryService/src/main/java/com/targaryentea/inventoryservice/service/InventoryService.java
+++ InventoryService/src/main/java/com/targaryentea/inventoryservice/service/InventoryService.java
@@ -1,22 +1,18 @@
 package com.targaryentea.inventoryservice.service;
 
 import com.targaryentea.inventoryservice.dto.InventoryResponse;
-import com.targaryentea.inventoryservice.entity.Inventory;
 import com.targaryentea.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
-
-    public InventoryService(InventoryRepository inventoryRepository) {
-        this.inventoryRepository = inventoryRepository;
-    }
 
     @Transactional(readOnly = true)
     public List<InventoryResponse> isInStock(List<String> skuCode) {
